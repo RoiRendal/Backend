ff59a3113b5027ca539708023315cb7e35d179d3
RoiRendal <23-16160@g.batstate-u.edu.ph>
2025-12-07 09:01:13 +0800

    Refactor authHandler for improved readability and error handling

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -2,30 +2,36 @@ import jwt from "jsonwebtoken";
 import * as UserModel from "../models/UserModel.js";
 
 const authHandler = async (req, res, next) => {
-    const {authorization} = req.headers;
+    const { authorization } = req.headers;
 
-    if(!authorization) {
-        res.status(401).json({
+    if (!authorization) {
+        return res.status(401).json({
             success: false,
             message: [
-                {result: 'You do not have permission to access this application.'}
+                { result: 'You do not have permission to access this application.' }
             ]
         });
     }
 
-    const token = authorization.split(' ')[1];
-
     try {
-        const {id} = jwt.verify(token, process.env.JWT_SECRET);
+        const token = authorization.split(' ')[1];
+
+        if (!token) {
+             return res.status(401).json({
+                success: false,
+                message: [{ result: 'Invalid token format.' }]
+            });
+        }
+
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
         const [user] = await UserModel.getUser(id);
-        /* req.user = user.id; */
 
         next();
-    } catch(e) {
-        res.status(401).json({
+    } catch (e) {
+        return res.status(401).json({
             success: false,
             message: [
-                {result: 'Unauthorized request.'}
+                { result: 'Unauthorized request.' }
             ]
         });
     }
